c12f5bb50dc6390f589215645c76c08a88b9899a
Telefonbuch Admin <admin@telefonbuch.app>
2025-06-02 17:03:04 +0200

    Fix: Vereinfache Routing-Konfiguration

--- next.config.js
+++ next.config.js
@@ -7,10 +7,6 @@ const nextConfig = {
     unoptimized: true
   },
   output: 'standalone',
-  trailingSlash: true,
-  experimental: {
-    serverActions: true
-  },
   distDir: '.next',
   async headers() {
     return [
@@ -22,14 +18,6 @@ const nextConfig = {
           { key: 'Access-Control-Allow-Methods', value: 'GET,POST,OPTIONS' },
           { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
         ]
-      }
-    ]
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: '/.netlify/functions/server/api/:path*'
       }
     ]
   }
